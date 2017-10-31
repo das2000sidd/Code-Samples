480dd55f7fa65ddd9e1b5b616a1a66cb5206955e
Siddhartha Das <das2000sidd@gmail.com>
2017-10-31 20:51:28 +0100

    Add files via upload

---
+++ decimalToBinaryAndBinaryToDecimal.py
@@ -0,0 +1,41 @@
+A1=120
+A2=33
+def decimalToBinary(anyNum):
+	binaryNum=""
+	while(anyNum!=0):
+		rem=anyNum%2
+		remStr=str(rem)
+		binaryNum=binaryNum+remStr
+		anyNum=anyNum//2
+	binaryNumReversed=""
+	
+	for i in range(len(binaryNum)-1,-1,-1):
+		binaryNumString=binaryNum[i]
+		binaryNumReversed=binaryNumReversed+ binaryNumString
+		
+	return binaryNumReversed
+	
+	
+print("Testing getting binary format of any decimal num")
+print(decimalToBinary(A1))
+print(decimalToBinary(A2))
+
+
+B1=1111000
+B2=100001
+def binaryToDecimal(binaryNum):
+	binaryNumAsStr=str(binaryNum)
+	decimalForm=0
+	lengthOfBinary=len(binaryNumAsStr)
+	for i in range(0,len(binaryNumAsStr)):
+		numAtPos=int(binaryNumAsStr[i])
+		decimalFormForNumAtPos=numAtPos*(2**(lengthOfBinary-i-1))
+		
+		decimalForm=decimalForm+decimalFormForNumAtPos
+	return decimalForm
+	
+	
+print("Testing getting decimal formay of any binary num")
+print(binaryToDecimal(B1))	
+print(binaryToDecimal(B2))
+	
